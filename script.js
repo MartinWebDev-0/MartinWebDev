ceabbe3ee1511aa30af63360deff315337177fa0
martin <martinfila49@gmail.com>
2025-01-30 17:33:55 +0100

    mq

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function () {
   const links = document.querySelector('.links');
   if (window.scrollY > 400) {
     links.style.display = 'none';
   } else {
     links.style.display = 'flex';
   }
-});*/
+});
 
 
 
